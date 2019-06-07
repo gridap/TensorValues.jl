494dc4dcabf20cccc64d3dc3bf0dd109e4c38e91
fverdugo <fverdugo@cimne.upc.edu>
2019-06-07 07:29:05 +0200

    Removed unnecessary @inbounds

--- src/Indexing.jl
+++ src/Indexing.jl
@@ -5,11 +5,11 @@ length(a::MultiValue) = length(a.array)
 
 @propagate_inbounds function getindex(
     a::MultiValue{S,T,N}, I::Vararg{Integer,N}) where {S,T,N}
-    @inbounds a.array[I...]
+    a.array[I...]
 end
 
 @propagate_inbounds function getindex(a::MultiValue, i::Integer)
-    @inbounds a.array[i]
+    a.array[i]
 end
 
 eltype(a::Type{MultiValue{S,T,N,L}}) where {S,T,N,L} = T
